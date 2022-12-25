6d31fc7c1e651894b78a5d12f4d96aebec1fd0a7
elaltas <elaltas@gmail.com>
2022-12-25 22:44:09 +0100

    refactor maven multimodule

--- discussion-1/README.md
+++ discussion-1/README.md
@@ -3,8 +3,7 @@ Work things
 We can use this to discuss small things.
 
 ## DISCUSSION 1
-Hi Tim,
-you can read the declared tests and we can figure it out by ourselves, :) 
+Mockito
 
 -------
 
--- discussion-1/src/test/java/com/example/workshits/controller/MyMercControllerTest2.java
+++ discussion-1/src/test/java/com/example/workshits/controller/MyMercControllerTest2.java
@@ -8,7 +8,6 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
-import org.springframework.beans.factory.annotation.Autowired;
 
 import static org.mockito.Mockito.when;
 
